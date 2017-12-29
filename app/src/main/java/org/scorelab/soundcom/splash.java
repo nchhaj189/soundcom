f66714302e17fe8932f76e09c945cf44cad8f83a
Nipun Chhajed <nipunchhajed@gmail.com>
2017-12-28 16:00:03 -0800

    added splash screen

--- app/src/main/java/org/scorelab/soundcom/splash.java
+++ app/src/main/java/org/scorelab/soundcom/splash.java
@@ -4,18 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class splash extends AppCompatActivity {
 
     // Splash screen timer
     private static int TIME_OUT = 4000;
-
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash_screen);
         getSupportActionBar().hide();
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(progressBar.VISIBLE);
 
         new Handler().postDelayed(new Runnable() {
 
